d48e2c5b85d56b7a5da82a3dee3ff3ce6b2ab419
xdumaster1 <145930645+xdumaster1@users.noreply.github.com>
2024-05-21 15:48:55 +0530

    Update __init__.py

--- custom_components/rako/__init__.py
+++ custom_components/rako/__init__.py
@@ -16,7 +16,6 @@ from .model import RakoDomainEntryData
 
 _LOGGER = logging.getLogger(__name__)
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Rako from a config entry."""
     rako_bridge = RakoBridge(
@@ -55,7 +54,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
 
     return True
-
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
