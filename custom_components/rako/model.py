f14bacd86eef872c21029d6da5d7c1d15d62e577
xdumaster1 <145930645+xdumaster1@users.noreply.github.com>
2024-05-22 11:03:37 +0530

    Delete custom_components/rako directory

--- custom_components/rako/model.py
+++
@@ -1,9 +0,0 @@
-from typing import TypedDict
-
-class RakoDomainEntryData(TypedDict):
-    rako_bridge_client: "RakoBridge"
-    rako_light_map: dict
-    rako_switch_map: dict
-    rako_cover_map: dict
-    rako_rgbw_map: dict
-    rako_listener_task: object
