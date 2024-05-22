576dce095c911ab6666a12d3ffe9a30171fa98cb
xdumaster1 <145930645+xdumaster1@users.noreply.github.com>
2024-05-22 10:40:56 +0530

    Update model.py

--- custom_components/rako/model.py
+++ custom_components/rako/model.py
@@ -1,17 +1,9 @@
-"""Rako shared models."""
-from __future__ import annotations
-
-from asyncio import Task
-from typing import TYPE_CHECKING, TypedDict
-
-if TYPE_CHECKING:
-    from .bridge import RakoBridge
-    from .light import RakoLight
-
+from typing import TypedDict
 
 class RakoDomainEntryData(TypedDict):
-    """A single Rako config entry's data."""
-
-    rako_bridge_client: RakoBridge
-    rako_light_map: dict[str, RakoLight]
-    rako_listener_task: Task | None
+    rako_bridge_client: "RakoBridge"
+    rako_light_map: dict
+    rako_switch_map: dict
+    rako_cover_map: dict
+    rako_rgbw_map: dict
+    rako_listener_task: object
