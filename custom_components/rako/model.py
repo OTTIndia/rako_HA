cb8f07514ec86411874bb599c461e58840061af5
xdumaster1 <145930645+xdumaster1@users.noreply.github.com>
2024-05-23 10:41:30 +0530

    Update model.py

--- custom_components/rako/model.py
+++ custom_components/rako/model.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING, TypedDict
 if TYPE_CHECKING:
     from .bridge import RakoBridge
     from .light import RakoLight
+    from .switch import RakoSwitch
 
 
 class RakoDomainEntryData(TypedDict):
@@ -14,4 +15,5 @@ class RakoDomainEntryData(TypedDict):
 
     rako_bridge_client: RakoBridge
     rako_light_map: dict[str, RakoLight]
+    rako_switch_map: dict[str, RakoSwitch]
     rako_listener_task: Task | None
