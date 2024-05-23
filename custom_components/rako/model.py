a8dc0d31fdc24be2578f37f964f458aeaa373e8e
xdumaster1 <145930645+xdumaster1@users.noreply.github.com>
2024-05-23 15:04:34 +0530

    Update model.py

--- custom_components/rako/model.py
+++ custom_components/rako/model.py
@@ -7,11 +7,14 @@ from typing import TYPE_CHECKING, TypedDict
 if TYPE_CHECKING:
     from .bridge import RakoBridge
     from .light import RakoLight
-
+    from .curtain import RakoCurtainEntity
+    from .rgbw_switch import RakoRGBWSwitchEntity
 
 class RakoDomainEntryData(TypedDict):
     """A single Rako config entry's data."""
 
     rako_bridge_client: RakoBridge
     rako_light_map: dict[str, RakoLight]
+    rako_curtain_map: dict[str, RakoCurtainEntity]
+    rako_rgbw_switch_map: dict[str, RakoRGBWSwitchEntity]
     rako_listener_task: Task | None
