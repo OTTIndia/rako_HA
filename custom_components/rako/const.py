302bfb3bd615727651c369a12c5b250fa037be3a
xdumaster1 <145930645+xdumaster1@users.noreply.github.com>
2024-05-23 18:43:10 +0530

    Delete custom_components directory

--- custom_components/rako/const.py
+++
@@ -1,2 +0,0 @@
-"""Constants for the Rako integration."""
-DOMAIN = "rako"
--- custom_components/rako/util.py
+++
@@ -1,7 +0,0 @@
-"""Utilities for Rako."""
-from __future__ import annotations
-
-
-def create_unique_id(bridge_id: str, room_id: int, channel_id: int) -> str:
-    """Create Unique ID for light."""
-    return f"b:{bridge_id}r:{room_id}c:{channel_id}"
